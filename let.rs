493130167f62aa395bbca3588d6e48ae357ec238
christopherleavitt99 <cml95@byui.edu>
2023-06-27 11:48:39 -0600

    Updated code

--- let.rs
+++ let.rs
@@ -1,3 +1,4 @@
+//Declare a Variable
 fn main1() {
     let company_string = "TutorialsPoint";  //string type
     let rating_float = 4.5;                 //float type
@@ -7,7 +8,13 @@ fn main1() {
     println!("rating is: {}",rating_float);
     println!("company is growing :{}",is_growing_boolean);
     println!("icon is: {}",icon_char);
-}
+} //Output should be:
+// company name is: TutorialsPoint
+// company rating on 5 is:4.5
+// company is growing: true
+// company icon is: ♥
+
+//Scalar Types
 fn main2() {
     let result = 10;    // i32 by default
     let age:u32 = 20;
@@ -17,7 +24,12 @@ fn main2() {
     println!("result value is: {}",result);
     println!("sum is {} and age is: {}",sum,age);
     println!("mark is: {} and count is {}",mark,count);
-}
+} //Output should be:
+// result value is 10
+// sum is -10 and age is 20
+// mark is 10 and count is 30
+
+//Integer Overflow
 fn main3() {
     let age:u8 = 255;
 
@@ -25,8 +37,13 @@ fn main3() {
     let weight:u8 = 256;    //overflow value is 0
     let height:u8 = 257;    //overflow value is 1
     let score:u8 = 258;     //overflow value is 2
-}
+}//Output should be:
+// age is 255
+// weight is 0
+// height is 1
+// score is 2
 
+//Float
 fn main4() {
     let result = 10.00;         //f64 by default
     let interest:f32 = 8.35;
@@ -35,4 +52,6 @@ fn main4() {
     println!("result value is {}",result);
     println!("interest is {}",interest);
     println!("cost is {}",cost);
-}
+}//Output should be:
+// interest is 8.35
+//cost is 15000.6
