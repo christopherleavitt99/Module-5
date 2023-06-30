579818039bbca2cb0539be46cb4dc180316ee944
christopherleavitt99 <cml95@byui.edu>
2023-06-29 18:23:53 -0600

    updated files so they could run

--- mutable.rs
+++ mutable.rs
@@ -1,8 +1,4 @@
 //Mutable
-let mut variable_name = value;
-let mut variable_name:dataType = value;
-Let us understand this with an example
-
 fn main() {
    let mut fees:i32 = 25_000;
    println!("fees is {} ",fees);
--- variables.rs
+++ variables.rs
@@ -1,8 +1,8 @@
 // Syntax
-let variable_name = value;          // no type specified
-let variable_name:datatype = value; //type specified
+// const variable_name = value;          // no type specified
+// const variable_name:datatype = value; //type specified
 fn main() {
-    let fees = 25_000;
+    let fees: i32 = 25_000;
     let salary:f64 = 35_000.00;
     println!("fees is {} and salary is {}",fees,salary);
 } //Output should be: fees is 25000 and salary is 35000.
